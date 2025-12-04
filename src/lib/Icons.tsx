00c457966da042a1898211fe6fd38b63503528fb
richard-nagy <big.ricsoo@gmail.com>
2025-12-04 11:35:47 +0100

    Fix some console errors

--- src/lib/Icons.tsx
+++ src/lib/Icons.tsx
@@ -13,27 +13,27 @@ const BookFinder = ({ size = 24, color = "currentColor", ...props }) => (
     >
         <path
             d="M11 22H5.5C4.83696 22 4.20107 21.7366 3.73223 21.2678C3.26339 20.7989 3 20.163 3 19.5C3 18.837 3.26339 18.2011 3.73223 17.7322C4.20107 17.2634 4.83696 17 5.5 17H10.001"
-            stroke-width="2.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <path
             d="M21 22L19.121 20.122"
-            stroke-width="2.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <path
             d="M3 19.5V4.5C3 3.83696 3.26339 3.20107 3.73223 2.73223C4.20107 2.26339 4.83696 2 5.5 2H18C18.2652 2 18.5196 2.10536 18.7071 2.29289C18.8946 2.48043 19 2.73478 19 3V11"
-            stroke-width="2.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
         <path
             d="M17 21C18.6569 21 20 19.6569 20 18C20 16.3431 18.6569 15 17 15C15.3431 15 14 16.3431 14 18C14 19.6569 15.3431 21 17 21Z"
-            stroke-width="2.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         />
     </svg>
 );
